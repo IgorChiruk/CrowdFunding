c1bb40fc75de9d0ce1e139ea9c0745312bdd3fc6
Igor Chiruk <mer13@tut.by>
2020-05-20 23:19:02 +0300

    start

---
+++ kraud/Models/Identity/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace kraud.Models
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("IdentityDb") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+}
